a406c98e6e831ff72c1aeb64350ef94d4a625c28
James Gooding <38703245+jameseg@users.noreply.github.com>
2024-07-02 15:51:32 -0400

    Add ui for the game
    add dom nodes
    add styling
    put messages in the nodes

--- main.js
+++ main.js
@@ -1,91 +1,93 @@
 function playGame() {
-  let computerScore = 0
-  let humanScore = 0
-
   function getComputerChoice() {
     const options = ["rock", "paper", "scissors"]
     const randomNumber = Math.floor((Math.random() * 3))
     const computerChoice = options[randomNumber]
 
-    console.log("computerChoice: " + computerChoice)
     return computerChoice
   }
 
   const buttons = document.querySelectorAll("button")
+  const humanScoreDiv = document.querySelector(".human-score")
+  const computerScoreDiv = document.querySelector(".computer-score")
+  const tiesDiv = document.querySelector(".ties")
+  const messageDiv = document.querySelector(".message > span")
+  const rulesAndEndMessage = document.querySelector(".rules")
+
+  humanScoreDiv.textContent = 0
+  computerScoreDiv.textContent = 0
+  tiesDiv.textContent = 0
+
 
   buttons.forEach(button => {
     button.addEventListener("click", (e) => {
       const humanChoice = e.target.textContent.toLowerCase()
-      console.log(humanChoice)
       playRound(humanChoice, getComputerChoice())
     })
-
   });
 
   // rounds played from human's perspective
   function playRound(humanChoice, computerChoice) {
-
-    let message = ""
     switch (humanChoice) {
       case "rock":
         if (computerChoice === "scissors") {
-          message = `YOU: ${humanChoice} | COMPUTER: ${computerChoice} You win! ${humanChoice} beats ${computerChoice} every time!`
-          humanScore++
-          alert(message)
+          messageDiv.textContent = `YOU: ${humanChoice} | COMPUTER: ${computerChoice}\n You win! ${humanChoice} beats ${computerChoice} every time!`
+          humanScoreDiv.textContent++
 
         } else if (computerChoice === "rock") {
-          message = `YOU: ${humanChoice} | COMPUTER: ${computerChoice} You tied :( ${humanChoice} is the same as ${computerChoice}.`
-          alert(message)
+          messageDiv.textContent = `YOU: ${humanChoice} | COMPUTER: ${computerChoice}\n You tied :( ${humanChoice} is the same as ${computerChoice}.`
+          tiesDiv.textContent++
         }
         else if (computerChoice === "paper") {
-          message = `YOU: ${humanChoice} | COMPUTER: ${computerChoice} You fool! why would you pick ${humanChoice} if the computer picked ${computerChoice}?`
-          computerScore++
-          alert(message)
+          messageDiv.textContent = `YOU: ${humanChoice} | COMPUTER: ${computerChoice}\n You fool! why would you pick ${humanChoice} if the computer picked ${computerChoice}?`
+          computerScoreDiv.textContent++
         }
         break;
       case "scissors":
         if (computerChoice === "scissors") {
-          message = `YOU: ${humanChoice} | COMPUTER: ${computerChoice} You tied :( ${humanChoice} is the same as ${computerChoice}.`
-          alert(message)
+          messageDiv.textContent = `YOU: ${humanChoice} | COMPUTER: ${computerChoice}\n You tied :( ${humanChoice} is the same as ${computerChoice}.`
+          tiesDiv.textContent++
         } else if (computerChoice === "rock") {
-          message = `YOU: ${humanChoice} | COMPUTER: ${computerChoice} You fool! why would you pick ${humanChoice} if the computer picked ${computerChoice}?`
+          messageDiv.textContent = `YOU: ${humanChoice} | COMPUTER: ${computerChoice}\n You fool! why would you pick ${humanChoice} if the computer picked ${computerChoice}?`
 
-          humanScore++
-          alert(message)
+          humanScoreDiv.textContent++
         }
         else if (computerChoice === "paper") {
-          message = `YOU: ${humanChoice} | COMPUTER: ${computerChoice} You win! ${humanChoice} beats ${computerChoice} every time!`
-          computerScore++
-          alert(message)
+          messageDiv.textContent = `YOU: ${humanChoice} | COMPUTER: ${computerChoice}\n You win! ${humanChoice} beats ${computerChoice} every time!`
+          computerScoreDiv.textContent++
         }
         break;
 
       case "paper":
         if (computerChoice === "scissors") {
-          message = `YOU: ${humanChoice} | COMPUTER: ${computerChoice} You fool! why would you pick ${humanChoice} if the computer picked ${computerChoice}?`
-          computerScore++
-          alert(message)
+          messageDiv.textContent = `YOU: ${humanChoice} | COMPUTER: ${computerChoice}\n You fool! why would you pick ${humanChoice} if the computer picked ${computerChoice}?`
+          computerScoreDiv.textContent++
         } else if (computerChoice === "rock") {
-          message = `YOU: ${humanChoice} | COMPUTER: ${computerChoice} You win! ${humanChoice} beats ${computerChoice} every time!`
-          humanScore++
-          alert(message)
+          messageDiv.textContent = `YOU: ${humanChoice} | COMPUTER: ${computerChoice}\n You win! ${humanChoice} beats ${computerChoice} every time!`
+          humanScoreDiv.textContent++
         }
         else if (computerChoice === "paper") {
-          message = `YOU: ${humanChoice} | COMPUTER: ${computerChoice} You tied :( ${humanChoice} is the same as ${computerChoice}.`
-          alert(message)
+          messageDiv.textContent = `YOU: ${humanChoice} | COMPUTER: ${computerChoice}\n You tied :( ${humanChoice} is the same as ${computerChoice}.`
+          tiesDiv.textContent++
         }
         break;
       default:
         return new Error(`You need to select ROCK, PAPER, or SCISSORS | you selected: ${humanChoice}`)
     }
-  }
 
-  if (humanScore > computerScore) {
-    alert(`You win the game! You had ${humanScore} and the machine had ${computerScore} - Nice job!`)
-  } else if (computerScore > humanScore) {
-    alert(`You got creamed. You had ${humanScore} and the machine had ${computerScore} - you need practice`)
-  } else {
-    alert("You tied the machine. Maybe play again?")
+
+    const body = document.querySelector("body")
+    if (humanScoreDiv.textContent === "5") {
+      body.style.backgroundColor = "lightgreen"
+      rulesAndEndMessage.textContent = `You win the game! You had ${humanScoreDiv.textContent} and the machine had ${computerScoreDiv.textContent} - Nice job!`
+      humanScoreDiv.textContent = 0
+      computerScoreDiv.textContent = 0
+    } else if (computerScoreDiv.textContent === "5") {
+      body.style.backgroundColor = "red"
+      rulesAndEndMessage.textContent = `You got creamed. You had ${humanScoreDiv.textContent} and the machine had ${computerScoreDiv.textContent} - you need practice`
+      humanScoreDiv.textContent = 0
+      computerScoreDiv.textContent = 0
+    }
   }
 }
 
